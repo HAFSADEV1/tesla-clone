09a95878e87728ee7200a26c39baf13d1819e1d8
hafsa chentouf <60079381+HAFSADEV1@users.noreply.github.com>
2021-06-09 08:04:55 +0100

    Update carSlice.js

--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,15 +1,15 @@
-// import {createSlice} from "@reduxjs/toolkit";
+ import {createSlice} from "@reduxjs/toolkit";
 
-// const intialState={
-//  cars : ["Model S","Model 3","Model X","Model Y"]
-// };
+const intialState={
+  cars : ["Model S","Model 3","Model X","Model Y"]
+ };
 
-// const carSlice=createSlice({
-//  name:"car",
-//  intialState,
-//  reducers:{}
-// });
+ const carSlice=createSlice({
+  name:"car",
+  intialState,
+  reducers:{}
+ });
 
-// export const selectCars = state => state.car.cars
+ export const selectCars = state => state.car.cars
 
-// export default carSlice.reducer
+ export default carSlice.reducer
